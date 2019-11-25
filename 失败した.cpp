5f00228968c75367ad3b495ce723a74246779b2d
zero-ven <42859616+zero-ven@users.noreply.github.com>
2019-11-25 22:52:39 +0800

    Create 失败した.cpp
    こんな人生は、无意味だった

---
+++ 失败した.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	for (int i=0;i<1000;i++)
+	{
+		cout << "失败した";
+	}
+
+	cout << endl<<"こんな人生は、无意味だった" << endl;
+
+	system("pause");
+	return 0;
+}
